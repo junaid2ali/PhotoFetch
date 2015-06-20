c173697b6717c429734506cd215e7cbbc539ec90
--add <jordan_li80@hotmail.com>
2015-06-20 12:06:06 -0400

    hide action bar in photolookupgrid

--- app/src/main/java/com/cestar/photofetch/PhotoLookupGridActivity.java
+++ app/src/main/java/com/cestar/photofetch/PhotoLookupGridActivity.java
@@ -5,8 +5,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -37,6 +35,8 @@ public class PhotoLookupGridActivity  extends AbsListViewBaseActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_lookup_grid);
+
+        getSupportActionBar().hide();
 
         //get bundle from previous activity
         Bundle extras = getIntent().getExtras();
@@ -127,31 +127,31 @@ public class PhotoLookupGridActivity  extends AbsListViewBaseActivity{
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_photo_lookup_grid, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent in = new Intent(PhotoLookupGridActivity.this, ViewImageActivity.class);
-            in.putExtra("IMAGE_URL",images.get(0).getPath());
-            in.putExtra("TITLE", images.get(0).getName());
-            in.putExtra("DESCRIPTION", images.get(0).getComments());
-            startActivity(in);
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_photo_lookup_grid, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            Intent in = new Intent(PhotoLookupGridActivity.this, ViewImageActivity.class);
+//            in.putExtra("IMAGE_URL",images.get(0).getPath());
+//            in.putExtra("TITLE", images.get(0).getName());
+//            in.putExtra("DESCRIPTION", images.get(0).getComments());
+//            startActivity(in);
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
 
     public void loadUpDB(){
